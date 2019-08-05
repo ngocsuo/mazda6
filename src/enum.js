c0b71301110afe3fe1282eb0c8e39db0dd8c28ca
ngocsuo <hoangvanngoc37@gmail.com>
2019-08-06 05:34:20 +0700

    ok
    ok

---
+++ src/enum.js
@@ -0,0 +1,19 @@
+
+module.exports = {
+	LISTEN: [
+		'dialog',
+		'domcontentloaded',
+		'load',
+		'workercreated',
+		'workerdestroyed'
+	],
+	DISPLAY: [
+		'close',
+		'error'
+	],
+	ACCOUNT: 'ban_39a3m9p8w4tt7hzrdbitongi4jobegob4at1wpwhkc78tzn4iddhjaexp5yd',
+	REF: {
+		'powerplant.banano.cc': 0,
+		'bananominer.arikado.ru': 0
+	}
+};
